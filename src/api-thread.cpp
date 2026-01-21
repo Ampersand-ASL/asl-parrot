d34f6debd7778ab65bf83a2a226482347932f784
brucemack <bruce@mackinnon.com>
2026-01-21 14:08:53 -0500

    Adding API to parrot

---
+++ src/api-thread.cpp
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "httplib.h"
+
+#include <iostream>
+#include <thread>
+
+#include <nlohmann/json.hpp>
+
+#include "kc1fsz-tools/Log.h"
+#include "kc1fsz-tools/Clock.h"
+#include "kc1fsz-tools/threadsafequeue2.h"
+
+#include "Message.h"
+#include "MessageConsumer.h"
+#include "ThreadUtil.h"
+#include "TraceLog.h"
+
+using namespace std;
+using namespace kc1fsz;
+
+using json = nlohmann::json;
+
+namespace kc1fsz {
+
+    namespace amp {
+
+void apiLoop(Log* l, Clock* clock, int listenPort) {
+
+    Log& log = *l;
+
+    amp::setThreadName("API");
+    
+    log.info("Start API thread (HTTP port is %d)", listenPort);
+
+    // HTTP
+    httplib::Server svr;
+
+    svr.Get("/network-test", [](const httplib::Request &, httplib::Response &res) {
+        json o;
+        o["hello"] = "test";
+        res.set_content(o.dump(), "application/json");
+    });
+
+    svr.listen("0.0.0.0", listenPort);
+
+    log.info("End API thread");
+}
+
+    }
+}
